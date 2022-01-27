9844cc3f80277d75459dfd0c52e235af661f4481
deashawnogle <deashawnogle@gmail.com>
2022-01-26 20:21:28 -0800

    add git / email on index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -15,27 +17,47 @@ const questions = [
         type: "input"
     },
     {
-        message: "Installation for the Project",
+        message: "Installation for the Project.",
         name: "installation",
         type: "input"
     },
     {
-        message: "Usage of the Project",
+        message: "Usage of the Project.",
         name: "usage",
         type: "input"
     },
     {
-        message: "Contribution of the Project",
+        message: "Contribution of the Project.",
         name: "contribution",
         type: "input"
     },
+    {
+        message: "Test instruction of the Project.",
+        name: "test",
+        type: "input"
+    },
+    {
+        message: "What is your Github account?",
+        name: "github",
+        type: "input"
+    },
+    {
+        message: "Provide your email address for further questions.",
+        name: "email",
+        type: "input"
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}`, data, (err) =>
+        err ? console.error('Error! : ' + err) : console.log('README generated'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => writeToFile('README.md', generateMarkdown(data)))
+}
 
 // Function call to initialize app
 init();
